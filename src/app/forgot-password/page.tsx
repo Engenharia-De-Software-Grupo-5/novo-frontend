4a53bae79d93fdf2cf12c281461ad67f46c7deae
vinicius-firmino35 <vinicius.candido.firmino@ccc.ufcg.edu.br>
2026-02-06 21:47:33 -0300

    feat: add login screens

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+import AuthCard from "@/features/components/auth/AuthCard"
+import ForgotPasswordForm from "@/features/components/auth/ForgotPasswordForm"
+
+export default function ForgotPasswordPage() {
+  return (
+    <AuthCard>
+      <ForgotPasswordForm />
+    </AuthCard>
+  )
+}
