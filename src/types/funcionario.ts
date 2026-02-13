59ea4c4056ba99574e692954895e93e49eca4710
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2026-02-13 12:22:17 -0300

    feat: create employee type

---
+++ src/types/funcionario.ts
@@ -0,0 +1,25 @@
+export interface EmployeeFile {
+  id: string;
+  name: string;
+  type: string;
+  size: number;
+  url: string;
+}
+
+export interface EmployeeSummary {
+  id: string;
+  name: string;
+  role: string;
+  status: 'Ativo' | 'Pendente' | 'Inativo';
+  lastContract?: EmployeeFile;
+}
+
+export interface EmployeeDetail extends EmployeeSummary {
+  email?: string;
+  cpf: string;
+  birthDate: string;
+  admissionDate?: string;
+  phone?: string;
+  address?: string;
+  Contracts?: EmployeeFile[];
+}
