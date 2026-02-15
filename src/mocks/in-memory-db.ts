2ec15f93d644465c54ae33e313c4d54cfa4cec3b
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2026-02-15 17:53:47 -0300

    feat: create type, mock and methods of condominium

--- src/mocks/in-memory-db.ts
+++ src/mocks/in-memory-db.ts
@@ -1,6 +1,8 @@
+import { mockCondominiums } from './condominiums';
 import { mockEmployeeDetails } from './employees';
 
 // "Database" in memory.
 // This allows persistence while the server is running.
 // Resetting the server (npm run dev) resets the data.
 export const employeesDb = [...mockEmployeeDetails];
+export const condominiumsDb = [...mockCondominiums];
